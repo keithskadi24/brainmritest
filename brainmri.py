5931d352e73b37cd48ffd5c5945e09bcf9e43a73
Keith Maravilla <112045695+keithskadi24@users.noreply.github.com>
2023-05-26 13:30:05 +0800

    Update brainmri.py

--- brainmri.py
+++ brainmri.py
@@ -1,36 +1,38 @@
 import streamlit as st
+import cv2
 import tensorflow as tf
+from tensorflow.keras.preprocessing import image
 import numpy as np
-from PIL import Image
 
 model = tf.keras.models.load_model('brainMRI.h5')
 
-def preprocess_image(image):
-    image = image.resize((128, 128))
-    image = np.array(image)
-    image = image.astype('float32') / 255.0
-    return image
+def preprocess_image(image_file):
+    img = cv2.imread(image_file)
+    img = cv2.resize(img, (128, 128))
+    img = img / 255.0
+    img = np.expand_dims(img, axis=0)
+    return img
 
 def main():
-    st.title("Brain Tumor MRI Classification")
-    st.write("Upload an MRI image to classify the tumor type.")
+    st.title("Brain Tumor Classification")
+    st.write("Upload an MRI image for tumor classification.")
 
-    # File upload
-    uploaded_file = st.file_uploader("Choose an MRI image", type=['jpg', 'jpeg', 'png'])
+    # Upload image file
+    uploaded_file = st.file_uploader("Choose an MRI image", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
-        image = Image.open(uploaded_file)
-        image = preprocess_image(image)
+        # Preprocess the image
+        img = preprocess_image(uploaded_file)
 
-        # Make prediction
-        predictions = model.predict(np.expand_dims(image, axis=0))
-        predicted_class = np.argmax(predictions[0])
+        # Make predictions
+        predictions = model.predict(img)
         class_names = ['notumor', 'glioma', 'meningioma', 'pituitary']
-        predicted_label = class_names[predicted_class]
+        predicted_class = class_names[np.argmax(predictions)]
 
-        # Display prediction
-        st.image(image, caption='Uploaded MRI Image', use_column_width=True)
-        st.write("Prediction:", predicted_label)
+        # Display the result
+        st.image(uploaded_file, caption='Uploaded MRI', use_column_width=True)
+        st.success(f"Predicted Class: {predicted_class}")
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
+
