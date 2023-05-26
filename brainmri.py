4c7d873ac36169d6c91038e3ff7e16e046b04351
Keith Maravilla <112045695+keithskadi24@users.noreply.github.com>
2023-05-26 14:07:22 +0800

    Update brainmri.py

--- brainmri.py
+++ brainmri.py
@@ -2,19 +2,18 @@ import streamlit as st
 import tensorflow as tf
 from PIL import Image, ImageOps
 import numpy as np
-from tensorflow import keras
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-    model = keras.models.load_model('brainMRI.h5')
+    model = tf.keras.models.load_model('brainMRI.h5')
     return model
 
 def import_and_predict(image_data, model):
-    size = (128, 128)
+    size = (256, 256)
     image = ImageOps.fit(image_data, size, Image.LANCZOS)
     image = np.asarray(image)
     image = image / 255.0
-    img_reshape = np.reshape(image, (1, 128, 128, 3))
+    img_reshape = np.reshape(image, (1, 256, 256, 3))
     prediction = model.predict(img_reshape)
     return prediction
 
