e6f62babcda9d92909b09ff01aa093446241064c
Keith Maravilla <112045695+keithskadi24@users.noreply.github.com>
2023-05-26 14:18:34 +0800

    Update brainmri.py

--- brainmri.py
+++ brainmri.py
@@ -2,6 +2,8 @@ import streamlit as st
 import tensorflow as tf
 from PIL import Image, ImageOps
 import numpy as np
+import requests
+from io import BytesIO
 
 @st.cache(allow_output_mutation=True)
 def load_model():
@@ -23,21 +25,36 @@ def import_and_predict(image, model):
 
 def main():
     st.title("Brain Tumor Classification")
-    file = st.file_uploader("Choose a brain MRI image", type=["jpg", "jpeg", "png"])
+    image_source = st.sidebar.selectbox("Select Image Source", ("Upload", "URL"))
 
-    if file is None:
-        st.text("Please upload an image file.")
-    else:
+    if image_source == "Upload":
+        file = st.sidebar.file_uploader("Choose a brain MRI image", type=["jpg", "jpeg", "png"])
+        if file is None:
+            st.sidebar.text("Please upload an image file.")
+            return
         image = Image.open(file)
-        st.image(image, caption='Uploaded MRI Image.', use_column_width=True)
-        model = load_model()
-        if st.button("Classify"):
-            prediction = import_and_predict(image, model)
-            categories = ['notumor', 'glioma', 'meningioma', 'pituitary']
-            class_index = np.argmax(prediction)
-            class_name = categories[class_index]
-            confidence = prediction[0][class_index]
-            st.success(f"Predicted Class: {class_name} (Confidence: {confidence:.2f})")
+
+    elif image_source == "URL":
+        image_url = st.sidebar.text_input("Enter image URL")
+        if image_url == "":
+            st.sidebar.text("Please enter an image URL.")
+            return
+        try:
+            response = requests.get(image_url)
+            image = Image.open(BytesIO(response.content))
+        except:
+            st.sidebar.text("Error: Failed to fetch the image from the provided URL.")
+            return
+
+    st.image(image, caption='Uploaded MRI Image.', use_column_width=True)
+    model = load_model()
+    if st.button("Classify"):
+        prediction = import_and_predict(image, model)
+        categories = ['notumor', 'glioma', 'meningioma', 'pituitary']
+        class_index = np.argmax(prediction)
+        class_name = categories[class_index]
+        confidence = prediction[0][class_index]
+        st.success(f"Predicted Class: {class_name} (Confidence: {confidence:.2f})")
 
 if __name__ == '__main__':
     main()
