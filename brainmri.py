ad75735ab962618233ae9db7b4972844d09a3c28
Keith Maravilla <112045695+keithskadi24@users.noreply.github.com>
2023-05-26 13:54:28 +0800

    Update brainmri.py

--- brainmri.py
+++ brainmri.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import tensorflow as tf
-from PIL import Image
+from PIL import Image, ImageOps
 import numpy as np
 
 @st.cache(allow_output_mutation=True)
@@ -9,33 +9,31 @@ def load_model():
     return model
 
 model = load_model()
+categories = ['notumor', 'glioma', 'meningioma', 'pituitary']
 
-st.title("Brain Tumor Classifier")
-file = st.file_uploader("Choose a brain MRI image", type=["jpg", "jpeg", "png"])
+st.title("Brain Tumor MRI Classification")
+file = st.file_uploader("Choose a brain MRI image", type=["jpg", "png"])
 
 def preprocess_image(image):
-    # Convert image to RGB if it's not already
-    if image.mode != "RGB":
-        image = image.convert("RGB")
-    
-    # Resize and normalize the image
-    image = image.resize((128, 128))
-    image = np.array(image) / 255.0
-    image = np.expand_dims(image, axis=0)
-    return image
+    resized_image = image.resize((128, 128))
+    normalized_image = np.array(resized_image) / 255.0
+    reshaped_image = np.reshape(normalized_image, (1, 128, 128, 3))
+    return reshaped_image
 
-@tf.function
-def predict(image, model):
+def classify_image(image, model):
     preprocessed_image = preprocess_image(image)
     prediction = model.predict(preprocessed_image)
-    return prediction
+    class_index = np.argmax(prediction)
+    class_name = categories[class_index]
+    confidence = prediction[0][class_index]
+    return class_name, confidence
 
 if file is None:
     st.text("Please upload an image file")
 else:
     image = Image.open(file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
-    prediction = predict(image, model)
-    class_names = ['notumor', 'glioma', 'meningioma', 'pituitary']
-    predicted_class = class_names[np.argmax(prediction)]
-    st.success(f"Predicted Class: {predicted_class} (Confidence: {prediction[0][np.argmax(prediction)]:.4f})")
+    st.image(image, use_column_width=True)
+    class_name, confidence = classify_image(image, model)
+    st.subheader("Prediction:")
+    st.write(f"Class: {class_name}")
+    st.write(f"Confidence: {confidence:.2f}")
